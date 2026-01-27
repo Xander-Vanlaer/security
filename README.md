126ee54a3cbdf52ae68bb4f7bdec6e83dd4a7893
Toon-unix <r1036008@student.thomasmore.be>
2026-01-27 15:36:37 +0100

    Merge branch 'main' of https://github.com/verrepty1223/SIL2.2_Frontend into WorkingBranch

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # SIL2.2_Frontend
 Frontend website for SIL2.2
+
+## Workflow
+
+1. Make a branch
+
+`git branch yv-example`
+
+2. Go into the branch
+
+`git checkout yv-example`
+
+3. Add, Commit, Push in the branch
+
+`git add .` 
+
+`git commit -m "text"`
+
+`git push`
+
+4. Then copy the given commaned and enter.
+
+`git push --set-upstream origin yv-example`
+
+5. Go to github in the repositoty in the tab 'Pull requests'
--- javascript/login.js
+++ javascript/login.js
@@ -1,6 +1,38 @@
 document.getElementById("loginForm").addEventListener("submit", async function (event) { 
     event.preventDefault();
     
+    //const username = document.getElementById("username").value;
+    //const password = document.getElementById("password").value;
+    //const p_invalid = document.getElementById("invalid");
+    //p_invalid.textContent = "";
+    //
+    //console.log("Username: ", username);
+    //console.log("Password: ", password);
+    //
+    //const list_users = ["Yan", "Toon", "Yannick", "Xander"];
+    //const list_passwords = ["231204yv", "test123!", "abc123!", "password"];
+    //
+    //console.log(list_users.includes(username));
+    //if (list_users.includes(username)) {
+    //    if (password == list_passwords[list_users.indexOf(username)]) {
+    //        console.info("Logged in");
+    //        getDashboard();
+    //    }
+    //    else {
+    //        console.error("Password: ", password, "not founded");
+    //        p_invalid.textContent = "Invalid: Wrong Username or password was given";
+    //    }
+    //}
+    //else {
+    //    console.error("Username: ", username, "not founded");
+    //    p_invalid.textContent = "Invalid: Wrong username or password was given";
+    //    
+    //}
+    
+    const buttonState = event.submitter;
+    const state = buttonState.value;
+    const list_users = ["Yan", "Toon", "Yannick", "Xander"];
+    const list_passwords = ["231204yv", "test123!", "abc123!", "password"];
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const p_username = document.getElementById("invalid_user");
